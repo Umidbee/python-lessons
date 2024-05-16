4b96fcf2043f7fec4d118f6f1bec1e3f73d2d0c5
Umidbek Xajiboyev <diyorsaratonov208@gmail.com>
2024-05-16 21:38:00 +0500

    yangi fayllar qo'shildi
    1 python dars yuklandi

---
+++ 1_lesson.py
@@ -0,0 +1,59 @@
+
+#print('Assalomu alekum')
+#print(2+4*2)
+#print(19/5)
+#print(2**4)
+#print('Men "Dell" markazidagi noutbuk sotib oldim')
+#print("""Odami ersang demagil odami,
+ #  Oniki, yoq xalq g'amidan g'ami""")
+
+#print("Nexia", "Tico", 'Damas', "ko'rganlar qilar havas")
+
+# O"zgaruvchilar lessons
+
+#words = 'Hello World'
+#print(words)
+
+#ism = 'Umidbek'
+
+#name = 'Ahad'
+#familya = 'Qayum'
+#print(name +  ' ' + familya)
+#ism = 'James'
+#last_name = 'Born'
+#ism_sharif = f"{ism} {last_name}"
+#print(ism_sharif)
+#name = 'james'
+#last_name = 'born'
+#ism_sharif = f"{name} {last_name}"
+#print(ism_sharif.upper())
+
+#meva = '    olma    '
+#print("Men " +  meva.strip() +  "yaxshi koraman")
+
+# Sonlar bn ishlash gooo
+
+#aholi_soni = 7_594_460_230
+#print('Aholi son', + aholi_soni)
+
+
+#bozorlik = ['yog', 'un', 'piyoz', 'kartoshka']
+
+#mahsulot = bozorlik.pop(1)
+#print(bozorlik)
+
+
+cars = ['mers', 'bmw', 'tesla', 'Rols-roys', 'gentra']
+cars.sort()
+print(cars)
+print(len(cars))
+
+#onlar = list(range(0, 10))
+#int(sonlar)
+
+#arxlar = [1200, 1300, 3000, 5600]
+#rint(narxlar)
+
+#rzon_narx = min(narxlar)
+#ng_qimmat_narx = max(narxlar)
+#rint("Eng qimmat narx:", eng_qimmat_narx, "Eng arzon narx:", arzon_narx)
