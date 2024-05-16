44bd95d3ce16220ca749982e24e225093bc4d760
Umidbek Xajiboyev <diyorsaratonov208@gmail.com>
2024-05-16 22:04:26 +0500

    2- dars python darsligi qoshildi

---
+++ 2_lesson-for.py
@@ -0,0 +1,9 @@
+mehmonlar = ['Salim', 'Umidbek', 'Azizbek', 'Sardorbek']
+for mehmon in mehmonlar:
+    print(mehmon.title())
+   
+ # if - else Tarmoqlanish 10 lessons
+ x = 20
+ y = 30
+if x < y:
+    print('Y katta X DAN'):
