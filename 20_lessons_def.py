31fe79dd557a029eb0639277ca3ca7b0f53c9b1b
Umidbek Xajiboyev <diyorsaratonov208@gmail.com>
2024-06-03 21:49:20 +0500

    funksiya darsligi qoshildi

---
+++ 20_lessons_def.py
@@ -0,0 +1,18 @@
+#  20 Dars Funksiyalar DEF
+# def salom_ber():
+#     print("Assalomu alekum")
+
+# salom_ber()
+
+
+# def salom_ber(ism):
+#     print(f"Asssalomu alekum hurmatli {ism.title()}")
+
+
+# salom_ber("hasan")
+# salom_ber("umidbek")
+# def tyiltop(ism, yosh):
+#     print(f"{ism} {2024-yosh} yoshda")
+
+
+# tyiltop("shahlo", 1983)
